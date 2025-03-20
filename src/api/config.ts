85660d93a82be98de8119b7439a4ef1e1717ee95
Mikhail Pshenichnyy <dremast1337@gmail.com>
2025-03-20 15:31:04 +0300

    test

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,6 +2,6 @@
 const API_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:3000/api';
 
 export default {
-    API_URL: 'http://localhost:3000',
-    BASE_URL: API_URL,
+    API_URL: 'http://falokut.ru:3000',
+    BASE_URL: 'http://falokut.ru:3000/api',
 }
